263307465e642e84abefeb2c2dbb2ca77efe8399
MortalAmongGods <krmcintosh87@gmail.com>
2024-03-04 20:22:22 -0500

    added images and playing with some styling

--- Invenium/src/app/about/about.component.ts
+++ Invenium/src/app/about/about.component.ts
@@ -1,12 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  aidianPhotos: string[] = ['assets/Images/Aidian_1.jpg'];
+  alexisPhotos: string[] = ['assets/Images/Alexis_1.jpg'];
+  colinPhotos: string[] = ['assets/Images/Colin_1.jpg'];
+  groupPhotos: string[] = ['assets/Images/Group_1.jpg', 'assets/Images/Group_2.jpg'];
+  mattPhotos: string[] = ['assets/Images/Matt_1.jpg'];
 
 }
--- Invenium/src/app/app.routes.ts
+++ Invenium/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { EventsComponent } from './events/events.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { MediaComponent } from './media/media.component';
 
 export const routes: Routes = [
     {path: 'Home', component: HomeComponent},
     {path: 'About', component: AboutComponent},
     {path: 'Events', component: EventsComponent},
     {path: 'Contact', component: ContactComponent},
+    {path: 'Media', component: MediaComponent},
     {path: '', redirectTo: 'Home', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent},
 ];
