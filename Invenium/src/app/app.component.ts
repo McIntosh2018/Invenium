5ecab89662c01dd1e23d8862a354dee18598f38e
MortalAmongGods <krmcintosh87@gmail.com>
2024-03-05 21:17:12 -0500

    changed format to be one long page

--- Invenium/src/app/app.component.ts
+++ Invenium/src/app/app.component.ts
@@ -4,17 +4,19 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AboutComponent } from "./about/about.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterBarComponent } from "./footer-bar/footer-bar.component";
+import { HomeComponent } from "./home/home.component";
+import { MediaComponent } from "./media/media.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, AboutComponent, RouterLink, RouterLinkActive, NavBarComponent, FooterBarComponent]
+    imports: [RouterOutlet, AboutComponent, RouterLink, RouterLinkActive, NavBarComponent, FooterBarComponent, HomeComponent, MediaComponent]
 })
 export class AppComponent {
   constructor() {
-    console.log(environment.production);
+    // console.log(environment.production);
   }
 
   title = 'Invenium';
--- Invenium/src/app/media/media.component.ts
+++ Invenium/src/app/media/media.component.ts
@@ -1,19 +1,34 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-media',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, HttpClientModule],
   templateUrl: './media.component.html',
   styleUrl: './media.component.css'
 })
 export class MediaComponent {
 
-  aidianPhotos: string[] = ['assets/Images/Aidian_1.jpg'];
-  alexisPhotos: string[] = ['assets/Images/Alexis_1.jpg'];
-  colinPhotos: string[] = ['assets/Images/Colin_1.jpg'];
-  groupPhotos: string[] = ['assets/Images/Group_1.jpg', 'assets/Images/Group_2.jpg'];
-  mattPhotos: string[] = ['assets/Images/Matt_1.jpg'];
+  // aidianPhotos: string[] = ['assets/Images/Aidian_1.jpg'];
+  // alexisPhotos: string[] = ['assets/Images/Alexis_1.jpg'];
+  // colinPhotos: string[] = ['assets/Images/Colin_1.jpg'];
+  // groupPhotos: string[] = ['assets/Images/Group_1.jpg', 'assets/Images/Group_2.jpg'];
+  // mattPhotos: string[] = ['assets/Images/Matt_1.jpg'];
+
+  allPhotos: string [] = [
+    'assets/Images/Aidan_1.jpg',
+    'assets/Images/Alexis_1.jpg',
+    'assets/Images/Colin_1.jpg',
+    'assets/Images/Group_1.jpg', 
+    'assets/Images/Group_2.jpg',
+    'assets/Images/Group_3.jpg',
+    'assets/Images/Matt_1.jpg'
+  ];
+  videos: string[] =['assets/Videos/Token_Lounge_1.mp4'];
 
 }
+
